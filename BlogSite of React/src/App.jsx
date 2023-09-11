c57d62c54d5c55d5b3bc825e8fd37f13e9b65742
turttlehouse <buidandrun464@gmail.com>
2023-09-11 18:09:03 +0545

    routing feature added: intalled react-router-dom

--- BlogSite of React/src/App.jsx
+++ BlogSite of React/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import AllBlogs from './pages/AllBlogs/AllBlogs'
+import SingleBlog from './pages/SingleBlog/SingleBlog'
+import CreateBlog from './pages/CreateBlog/CreateBlog'
+import EditBlog from './pages/EditBlog/EditBlog'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AllBlogs/>}  />
+      <Route path='/singleBlog' element={<SingleBlog/>} />
+      <Route path = '/createBlog' element={<CreateBlog/>}  />
+      <Route path= '/editBlog' element = {<EditBlog/>}  />
+
+     
+    </Routes>
+    </BrowserRouter>
   )
 }
 
