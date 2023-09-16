eb4bf3f42f10bb78156b31d4f9020fd856a504cf
turttlehouse <buidandrun464@gmail.com>
2023-09-16 07:47:04 +0545

    card added for blog in Allblogs.jsx  width,height,margin-top property modified in Allblogs.css

--- BlogSite of React/src/pages/AllBlogs/AllBlogs.jsx
+++ BlogSite of React/src/pages/AllBlogs/AllBlogs.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-
+import './AllBlogs.css'
 
 
 const AllBlogs = () => {
   return (
     <div>
         <Navbar/>
-        AllBlogs</div>
+
+         <div className="card">
+             <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"  width="100%"/>
+              <div className="container">
+                <h4><b>John Doe</b></h4> 
+                <p>Architect & Engineer</p> 
+              </div>
+          </div>
+    </div>
   )
 }
 
