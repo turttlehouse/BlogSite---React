3932a10f6135b58c4d105a3e77e665cc010f72e9
turttlehouse <buidandrun464@gmail.com>
2023-09-16 08:30:52 +0545

    Third Party package installed-axios for fetching API data from MockAPI and useState container created for storing those JSON format data and SetBlogs for setting those data inside the container and map is used for looping the card using the unique primary key id

--- BlogSite of React/src/pages/AllBlogs/AllBlogs.jsx
+++ BlogSite of React/src/pages/AllBlogs/AllBlogs.jsx
@@ -1,20 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import './AllBlogs.css'
+import axios from "axios"
 
 
 const AllBlogs = () => {
+
+
+  const [blogs,setBlogs] = useState([])
+
+
+  const fetchBlogs=async()=>{
+    const response = await axios.get("https://650508ffef808d3c66efb88c.mockapi.io/blogs")
+    // console.log(response)
+    // console.log(response.data)
+    if(response.status==200)
+    {
+      console.log(response.data)
+      setBlogs(response.data)
+    }
+
+    
+
+
+
+  }
+
+
+
+  useEffect(()=>{
+    // console.log("hello")
+    fetchBlogs()
+
+
+  },[])
+
+
+
   return (
     <div>
         <Navbar/>
 
-         <div className="card">
-             <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"  width="100%"/>
-              <div className="container">
-                <h4><b>John Doe</b></h4> 
-                <p>Architect & Engineer</p> 
-              </div>
-          </div>
+        <div style={{display:"flex",justifyContent:"space-evenly",flexWrap:"wrap"}}>
+          {
+            blogs.map((blog)=>{
+              return(
+                <div className="card" key={blog.id}>
+                <img src={blog.avatar} alt="Avatar"  width="100%"/>
+                <div className="container">
+                     <h4><b>{blog.title}</b></h4> 
+                     <p style={{color:"red"}}>{blog.description}</p> 
+                     <p>{blog.createdAt}</p>
+                </div>
+            </div>
+
+              )
+               
+              
+            })
+          }
+
+        </div>
+
+         
     </div>
   )
 }
