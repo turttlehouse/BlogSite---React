c49b8619184b95cda8b2c8c61fa76b452db48a56
turttlehouse <buidandrun464@gmail.com>
2023-09-16 16:00:52 +0545

    Navbar.jsx- add blog option added in navbar and Creatblog.jsx - form made to create a blog and to track the each field useState is used and post method is used to send data to backend, after form submission js preventdefault function and useNavigate hooks is used to avoid reloads of page

--- BlogSite of React/src/components/Navbar/Navbar.jsx
+++ BlogSite of React/src/components/Navbar/Navbar.jsx
@@ -6,10 +6,9 @@ const Navbar = () => {
   return (
     <nav className="navbar">
     <ul className="nav-list">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/createBlog">AddBlog</a></li>
+        
     </ul>
 </nav>  )
 }
--- BlogSite of React/src/pages/CreateBlog/CreateBlog.jsx
+++ BlogSite of React/src/pages/CreateBlog/CreateBlog.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './CreateBlog.css'
+import Navbar from '../../components/Navbar/Navbar'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreateBlog = () => {
+
+  const navigate = useNavigate()
+
+  const[title,setTitle] = useState("")
+  const[description,setDescription]=useState("")
+  const[image,setImage]=useState("")
+
+  // console.log(title)
+
+  const createBlog=async(e)=>{
+    e.preventDefault()
+
+    const data ={
+      title: title,
+      description : description,
+      avatar  : image
+    }
+
+    const response = await axios.post("https://650508ffef808d3c66efb88c.mockapi.io/blogs/",data)
+     
+    // console.log(response)
+    if(response.status==201){
+      // console.log(response.data)
+      // setBlogs(response.data)
+
+      //window.location.href="/"    //yesle reload hunxa submit garera home page ma redirect garda tesaile useNavigate vanne hooks use gareko ho
+
+      navigate("/")
+    }
+    else{
+      alert("something went wrong")
+    }
+
+  }
+
+
   return (
-    <div>CreateBlog</div>
+    <div className="container">
+      <Navbar/>
+    <h1 className="form-title">Add Blog</h1>
+    
+    <form onSubmit={createBlog}>
+        <input type="text" id="title" placeholder="title" name="title" required onChange={(e)=>setTitle(e.target.value)} />
+
+        <textarea id="description" placeholder="description" name="description" rows="4" required onChange={(e)=>setDescription(e.target.value)}></textarea>
+
+        <input type="text" id="image" placeholder="image" name="image" accept="image/*" required onChange={(e)=>setImage(e.target.value)}/>
+
+        <input type="submit" value="Submit"/>
+    </form>
+</div>
   )
 }
 
